c3cabded07fc19602da485679b4d9497caddfdb2
DevNadeem <khanjees@hotmail.com>
2016-02-22 03:47:26 +0500

    update
    update

---
+++ www/js/Controller/mainPageCtrl.js
@@ -0,0 +1,41 @@
+atyaatAppModule.controller('mainPageController',function($scope , $ionicSideMenuDelegate,$ionicPopup,$http, $state ){
+  $scope.showbutton=false
+
+  $scope.sideMenu = function(){
+    $ionicSideMenuDelegate.toggleLeft();
+  }
+  $scope.fields=[
+    "عطیات","صدقات","زکوٰۃ","فدیہ","فطرہ","تعمیِر مسجد",
+   ];
+  $scope.fieldIndex = function (index){
+    $scope.showbutton = true;
+    //console.log(index)
+    if(index == 0){
+      $state.go('atyat')
+
+    }
+    else if(index == 1){
+    $state.go('sadqat')
+
+    }
+    else if(index == 2){
+      $state.go('zakat')
+
+    }
+    else if(index == 3){
+      $state.go('fidya')
+
+    }
+    else if(index == 4){
+      $state.go('fitra')
+
+    }
+    else if(index == 5){
+      $state.go('taamer-e-masjid')
+
+    }
+  }
+
+
+
+});
